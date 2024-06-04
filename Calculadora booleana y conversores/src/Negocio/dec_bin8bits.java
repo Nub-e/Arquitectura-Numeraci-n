f5f5f80c92909063d0254bc05c63d4204e3091fc
Sauce-Boy <misterswagx@gmail.com>
2024-06-03 20:32:42 -0500

    Actualizacion del codigo

--- Calculadora booleana y conversores/src/Negocio/dec_bin8bits.java
+++ Calculadora booleana y conversores/src/Negocio/dec_bin8bits.java
@@ -4,73 +4,66 @@ package Negocio;
  * @author asus
  */
 public class dec_bin8bits {
-    private int numero;
+    
+    private int numero;  // Cambiar a entero para asegurar una representación adecuada en binario
 
     public dec_bin8bits(int numero) {
         this.numero = numero;
     }
-    
-    
-    public String binario(){
+
+    // Método para obtener la representación binaria de un número entero en 8 bits
+    public String binario() {
         StringBuilder bin = new StringBuilder();
-        Dec_bin binario = new Dec_bin (numero);
-        binario.separar();
-        String numero = binario.entero();
-        for(int i = 0; i < numero.length();i++){
-            bin.append(numero.charAt(i));
+        int num = Math.abs(numero);
+        while (num > 0) {
+            bin.append(num % 2);
+            num /= 2;
         }
-        while(bin.length() < 7){
-            bin.insert(0,'0');
+        while (bin.length() < 8) {
+            bin.append('0');
         }
-        return bin.toString();
+        return bin.reverse().toString();
     }
-    
-    public String complemento1(String binario){
+
+    // Método para calcular el complemento a uno
+    public String complemento1(String binario) {
         StringBuilder complemento = new StringBuilder();
-        for(char bin : binario.toCharArray()){
+        for (char bin : binario.toCharArray()) {
             complemento.append(bin == '0' ? '1' : '0');
         }
         return complemento.toString();
     }
-    
-   public String complemento2(String complemento1){
-       StringBuilder comp2 = new StringBuilder();
-       boolean acarreo = true;
-       for(int i = complemento1.length() - 1; i>=0 ; i--){
-           char bit = complemento1.charAt(i);
-           if(acarreo){
-               if(bit == '1'){
-                   comp2.append('0');
-               }else{
-                   comp2.append('1');
-                   acarreo = false;
-               }
-           }else{
-               comp2.append(bit);
-           }
-       }if(acarreo){
-           comp2.append('1');
-       }
-       return comp2.reverse().toString();
-   } 
-   
-   public boolean signo(){
-        int n = numero;
-        if(n < 0){
-            return true;
-        }else{
-            return false;
+
+    // Método para calcular el complemento a dos
+    public String complemento2(String complemento1) {
+        StringBuilder comp2 = new StringBuilder();
+        boolean acarreo = true;
+        for (int i = complemento1.length() - 1; i >= 0; i--) {
+            char bit = complemento1.charAt(i);
+            if (acarreo) {
+                if (bit == '1') {
+                    comp2.append('0');
+                } else {
+                    comp2.append('1');
+                    acarreo = false;
+                }
+            } else {
+                comp2.append(bit);
+            }
         }
+        if (acarreo) {
+            comp2.append('1');
+        }
+        return comp2.reverse().toString();
     }
-   
-   public String imprimir(){
-       if(signo() == false){
+
+    // Método para imprimir la representación binaria o su complemento a dos según el signo del número
+    public String imprimir() {
+        if (numero >= 0) {
             return "El numero es positivo y su complemento a 2 es: " + binario();
-        }else{
+        } else {
             return "El numero es negativo y su complemento a 2 es: " + complemento2(complemento1(binario()));
         }
-   }
-  
-   
-   
+    }
 }
+
